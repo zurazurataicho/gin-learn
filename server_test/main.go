d5d2c9a1e1802b46c26ea0526f563fcb68f23e7e
EZURA, Atsushi <zurazurataicho@gmail.com>
2024-12-22 15:18:04 +0900

    init

---
+++ server_test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/hoge", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "fuga"})
+	})
+	r.Run()
+}
